c3d48ce7a9dbfd958c11caf63b9531447f5ea625
Kirill A. Korinskiy <catap@catap.ru>
2015-02-21 22:43:04 +0700

    Solve the 10 problem

---
+++ src/main/scala/Problems/p10.scala
@@ -0,0 +1,12 @@
+package Problems
+
+object p10 extends Problem {
+
+  val max = 2000000
+
+  lazy val primes = p7.prime.takeWhile(_ <= max)
+
+  lazy val sum = primes.map(_.toLong).sum
+
+  override def solve(): String = sum.toString
+}
