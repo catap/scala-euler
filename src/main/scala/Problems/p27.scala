d21deceeeb192bd32dadb94dde1c43d9ee84b5e5
Kirill A. Korinskiy <catap@catap.ru>
2015-02-24 10:50:56 +0700

    Performance problem 27: from 23s 353ms to 1s 148ms

--- src/main/scala/Problems/p27.scala
+++ src/main/scala/Problems/p27.scala
@@ -5,10 +5,14 @@ object p27 extends Problem {
   lazy val prime: Stream[Int] =
     2 #:: Stream.from(3).filter(i => prime.takeWhile(j => i >= (j * j)).forall(i % _ > 0))
 
-  def isPrime(n: Int) = prime.takeWhile(_ <= n).contains(n)
+  lazy val primes = prime.takeWhile(_ < 1000).map(p => p -> p).toMap
 
-  lazy val (a, b, _) = (-999 to 999).flatMap { a =>
-    (-999 to 999).map(b => (a, b, (0 to 1000)
+  lazy val nprimes = primes.keys ++ primes.keys.map(_ * -1)
+
+  def isPrime(n: Int) = primes.contains(n)
+
+  lazy val (a, b, n) = (-999 to 999).flatMap { a =>
+    nprimes.map(b => (a, b, (0 to 1000)
       .takeWhile(n => isPrime(n * n + a * n + b)).size))
   }.reduce((a, b) => if(a._3 > b._3) a else b)
 
