d356b59d65350428f21808ea7ff664487d0ef205
Kirill A. Korinskiy <catap@catap.ru>
2015-02-24 10:51:59 +0700

    Problem 27 is solved

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ import scala.reflect.runtime.universe
 
 object Main extends App {
 
-  val last_solved_problem = 26
+  val last_solved_problem = 27
 
   val runtimeMirror = universe.runtimeMirror(getClass.getClassLoader)
 
